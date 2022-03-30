c9e890b649fa1c4496fcd1fe7354fbc02e3938ad
magarc82 <miguelagp1307@gmail.com>
2022-03-29 18:43:31 -0700

    added scatter data and main

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+var anime_data
+var connection_data
+document.addEventListener('DOMContentLoaded', () => {
+    // Load both files before doing anything else
+    Promise.all([d3.json('data/anime.csv'),
+                 d3.csv('data/preproccesed_final_connections.csv')])
+                 .then(function(values){
+
+        
+        anime_data = values[0];
+        connection_data = values[1];
+
+        anime_data.forEach(d => {
+            d.Year = +d["episodes"]
+            d["rating"] = +d["rating"]
+            d["members"] = +d["members"]
+        });
+        console.log(schoolData)
+        tooltip = d3.select('#tooltip').style("opacity", 0)
+       
+    });
+});
+function get_data(){
+
+    return [anime_data, connection_data]
+
+}
 function main()
 {
     
--- js/scatterPlot.js
+++ js/scatterPlot.js
@@ -1,4 +1,29 @@
+import get_data from "./main"
+var scatterSvg;
+var lineWidth, lineHeight, lineInnerHeight, lineInnerWidth;
+var lineMargin = { top: 50, right: 60, bottom: 60, left: 100 };
+var data_scatter = get_data()[0]
+
 function scatterPlotInit()
 {
+    scatterSvg = d3.select('#scatterPlotSVG');
+    scatterSvg.selectAll('*').remove();
+    console.log(data_scatter)
+
+//     max_val = d3.max(school_data_state, function(d){
+//         return d[attribute]
+//     })
+
+//     min_val = d3.min(school_data_state, function(d){
+//     return d[attribute]
+// })
+//     x_scale = d3.scaleTime()
+//     .domain([start_year,end_year])
+//     .range([0, innerWidth]);
+
+//     y_scale = d3.scaleLinear()
+//     .domain([min_val,max_val])
+//     .range([innerHeight - margin.bottom-margin.top , 0]);
+
     
 }
