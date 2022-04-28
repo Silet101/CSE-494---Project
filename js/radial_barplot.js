402773e2e94c8eab53d9e0c3daad70c962921212
afrost5 <61331960+afrost5@users.noreply.github.com>
2022-04-28 00:55:30 -0700

    Detailed Radial Plot

--- js/radial_barplot.js
+++ js/radial_barplot.js
@@ -4,6 +4,10 @@ function drawRadialBarPlot()
     anime_data;
     genre_list;
 
+    let angles = [-90.5, -82, -73.3, -64.8, -56.4, -47.9378, -39.3254, -30.7129, -22.1005, -13.488, -4.8756, 3.73684, 12.3493, 20.9617, 
+        29.4, 38.0, 46.6, 55.1, 63.8, 72.4, 80.8, 89.5, 98.0, 106.5, 115.2, 123.8, 132.5, 141.2, 149.7, 158.3, 167.0, 175.1, 184.0, 193.0, 
+        201.8, 210.2, 218.4, 227.0, 235.5, 243.885, 252.498, 261.11];
+
     //Initialize the genre map
     genre_map = new Map(genre_list.map(data => [data.genre, 0]));
 
@@ -47,7 +51,8 @@ function drawRadialBarPlot()
                     .attr('y', 335) //Radius of circle
                     .attr('width', 10) 
                     .attr('height', (d) => y_axis(genre_map.get(d)))
-                    .style('transform', (d) => `translate(350px, 350px)rotate(${x_axis(d) - 91}deg)`) //Translate moves the circle!
+                    .style('transform', (d, i) => `translate(350px, 350px)rotate(${angles[i]}deg)`) //Translate moves the circle!
                     .attr('id', (d) => d)
                     .attr('fill', 'steelblue');
+
 }
